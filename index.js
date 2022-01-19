bce57dd7823e459b56517682e90ed979bccd39e9
NutsR <pymd.arc@gmail.com>
2022-01-19 13:09:33 +0530

    fixing

--- index.js
+++ index.js
@@ -62,7 +62,9 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
 
 	app.get("*", function (req, res) {
-		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+		res.sendFile(
+			path.join(__dirname, "client/build", "client/build/index.html")
+		);
 	});
 }
 app.use("/api", registerRoutes, userRoutes, serverRoutes);
