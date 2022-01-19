6889a73efaa8dfd53ff036bfbc8aaec6c497ee7b
NutsR <pymd.arc@gmail.com>
2022-01-19 14:34:44 +0530

    d

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ io.on("connection", (socket) => {
 	});
 });
 if (process.env.NODE_ENV === "production") {
-	console.log("hi");
+	console.log("hi1");
 	app.use(express.static(path.join(__dirname, "client/build")));
 
 	app.get("*", function (req, res) {
