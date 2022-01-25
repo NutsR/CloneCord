1fe5e8c54156f632b1f1d9e9c38b544ddccaabc1
NutsR <pymd.arc@gmail.com>
2022-01-25 21:08:56 +0530

    fixing heroku"

--- client/src/hooks/socket.io.context.js
+++ client/src/hooks/socket.io.context.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { io } from "socket.io-client";
-import parser from "socket.io-msgpack-parser";
+const parser = require("socket.io-msgpack-parser");
 const url = process.env.REACT_APP_public_url;
 
 export const socket = io({ autoConnect: false, parser });
